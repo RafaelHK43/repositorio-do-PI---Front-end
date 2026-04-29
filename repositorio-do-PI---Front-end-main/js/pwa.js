ea7d720dce38f62497bc32f109e8220417079874
Gustavo <gustavobmoura3z9@gmail.com>
2026-04-29 08:21:21 -0300

    Delete repositorio-do-PI---Front-end-main directory

--- repositorio-do-PI---Front-end-main/js/pwa.js
+++
@@ -1,7 +0,0 @@
-export function registerPWA() {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("./service-worker.js").catch(() => {});
-    });
-  }
-}
